fc21cb2ac293782e33c7acf02d87d03d72d28231
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-10 00:43:56 -0300

    criei o exeercício de while e do

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+/*ESTRUTURA DE REPETIÇÃO WHILE(ENQUANTO) - TESTE LÓGICO NO INÍCIO
+    var contador = 1
+    while (contador <= 6) {
+        console.log(`Passo ${contador}`)
+        contador++
+    }
+*/
+/*
+ESTRUTURA DE REPETIÇÃO DO(FAÇA) - TESTE LÓGICO NO FINAL
+
+var contador = 1
+    do {
+        console.log(`Passo ${contador}`)
+        contador++
+    } while (contador <= 6)
+    
+
+        
+        
+       
+
+/* 
+OS CÓDIGOS ACIMA OBTEEM O RESULTADO ABAIXO, SEM TER QYE ESCREVER MUITAS LINHAS 
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+*/
