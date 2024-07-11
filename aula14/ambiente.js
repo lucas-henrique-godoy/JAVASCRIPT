7ae29f7561fc420a9c064141172d8060c18b73d5
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-11 00:52:46 -0300

    criei o exemplo de loop for com javascript

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,4 +1,7 @@
-/*  WHILE
+/*  EXEMPLOS CÓDIGOS EM DIFERENTES ESTRUTURAS QUE FAZEM A MESMA COISA
+
+
+    WHILE
 var c= 1 
 while (c <= 5) {
     console.log(c)
@@ -14,6 +17,8 @@ do {
 } while(c <= 5)
 */
 
+//----------------------------------------
+//--- FOR ---// 
 console.log('Vai começar... ')
 for (var c = 1; c <= 5; c++) {
     console.log(c)
