fa7c1ac52f32df677041d77e0e856205b0e4261d
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-11 00:48:09 -0300

    modifiquei umas coisas

---
+++ aula14/ambiente.js
@@ -0,0 +1,24 @@
+/*  WHILE
+var c= 1 
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+//----------------------------------------
+* DO WHILE
+var c = 1
+do {
+  console.log(c)
+    c++  
+} while(c <= 5)
+*/
+
+console.log('Vai começar... ')
+for (var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+console.log('FIM! ')
+
+
+
--- aula14/teste.js
+++
@@ -1,4 +0,0 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
