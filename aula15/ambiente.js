03b831c0963c72ede108d89bcc1e894743777e31
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-07-14 02:09:34 -0300

    funções para iterar em arrays em javascript

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -3,8 +3,15 @@
 // A ordem dos comandos influencia nos resultados.
 
 let num = [5, 8, 2, 9, 3];  // Criação do array 'num' com os valores 5, 8, 2, 9, 3
-num.sort();                // Ordena o array 'num' em ordem crescente: [2, 3, 5, 8, 9]
-num.push(1);               // Adiciona o valor 1 ao final do array 'num': [2, 3, 5, 8, 9, 1]
-console.log(num);          // Imprime o array 'num' no console: [2, 3, 5, 8, 9, 1]
+num.push(1);               // Adiciona o valor 1 ao final do array 'num': [5, 8, 2, 9, 3, 1]
+num.sort();                // Ordena o array 'num' em ordem crescente: [1, 2, 3, 5, 8, 9]
+console.log(num);          // Imprime o array 'num' no console: [1, 2, 3, 5, 8, 9]
 console.log(`O vetor tem ${num.length} posições`);  // Imprime a quantidade de elementos no array 'num'
 console.log(`O primeiro valor do vetor é ${num[0]}`);  // Imprime o primeiro valor do array 'num'
+
+let pos = num.indexOf(4);  // Procura o valor 4 no array 'num'
+if (pos == -1) {           // Verifica se o valor 4 não foi encontrado
+    console.log('O valor não foi encontrado!');
+} else {                   // Caso contrário, imprime a posição do valor 4 no array
+    console.log(`O valor 4 está na posição ${pos}`);
+}
--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,4 +1,5 @@
 let valores = [8, 1, 7, 4, 2, 9]
+valores.sort()
 
 //console.log(valores) FORMA PADRÃO
 /*
@@ -12,7 +13,17 @@ console.log(valores[5])
 
 
 //MODO TRADICIONAL DE PERCURSO EM VETORES - FORMA PERSONALIZADA: Aqui é possível formatar a saída.
-
-for (let pos = 0; pos < valores.length; pos++) {
+/*for (let pos = 0; pos < valores.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
+*/
+
+
+// FORMA MAI SIMPLIFICADA E OTIMIZADA PARA VARIAVEIS COMPOSTAS E OBJETOS
+for (let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+
+
+
